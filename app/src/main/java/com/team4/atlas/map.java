41a93dc3e46152a2fb5aa12d585748c44b89bd34
z5117298 <v.shen@student.unsw.edu.au>
2018-02-16 17:31:19 +1100

    copied android files over

---
+++ app/src/main/java/com/team4/atlas/map.java
@@ -0,0 +1,13 @@
+package com.team4.atlas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class map extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+}
