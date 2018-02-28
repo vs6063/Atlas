2f866de6c59b1c24bf6131590f355debf9f54b67
z5117298 <v.shen@student.unsw.edu.au>
2018-02-28 19:01:14 +1100

    added scrollable for building information

--- app/src/main/java/com/team4/atlas/BuildingActivity.java
+++ app/src/main/java/com/team4/atlas/BuildingActivity.java
@@ -1,7 +1,10 @@
 package com.team4.atlas;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class BuildingActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class BuildingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_building);
+
+        ImageButton backButton = (ImageButton) findViewById(R.id.back);
+        backButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                Intent intent = new Intent(BuildingActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
