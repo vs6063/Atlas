9fde85cdf4ed933d6c3cb45a51734cdf7fa9498f
Victor Shen <victor21898@hotmail.com>
2018-03-02 14:09:46 +1100

    fixed 2d scrolling for unsw map

--- app/src/main/java/com/team4/atlas/MainActivity.java
+++ app/src/main/java/com/team4/atlas/MainActivity.java
@@ -9,8 +9,19 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.widget.HorizontalScrollView;
+import android.widget.ScrollView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private float mx, my;
+    private float curX, curY;
+
+    private ScrollView vScroll;
+    private HorizontalScrollView hScroll;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +31,8 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
 
-        // get intent for search, verify action and get the query
-        Intent intent = getIntent();
-        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-            String query = intent.getStringExtra(SearchManager.QUERY);
-            doMySearch(query);
-        }
+        vScroll = (ScrollView) findViewById(R.id.vScroll);
+        hScroll = (HorizontalScrollView) findViewById(R.id.hScroll);
 
         ImageButton spaceButton = (ImageButton) findViewById(R.id.space);
         spaceButton.bringToFront();
@@ -57,9 +64,45 @@ public class MainActivity extends AppCompatActivity {
         ImageButton location = (ImageButton) findViewById(R.id.location);
         scatterPlot.bringToFront();
         location.bringToFront();
+
+        // get intent for search, verify action and get the query
+        /*
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            doMySearch(query);
+        }
+        */
     }
 
-    void doMySearch(String query) {
+    //void doMySearch(String query) {}
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float curX, curY;
+
+        switch (event.getAction()) {
+
+            case MotionEvent.ACTION_DOWN:
+                mx = event.getX();
+                my = event.getY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                curX = event.getX();
+                curY = event.getY();
+                vScroll.scrollBy((int) (mx - curX), (int) (my - curY));
+                hScroll.scrollBy((int) (mx - curX), (int) (my - curY));
+                mx = curX;
+                my = curY;
+                break;
+            case MotionEvent.ACTION_UP:
+                curX = event.getX();
+                curY = event.getY();
+                vScroll.scrollBy((int) (mx - curX), (int) (my - curY));
+                hScroll.scrollBy((int) (mx - curX), (int) (my - curY));
+                break;
+        }
+
+        return true;
     }
 }
---
+++ app/src/main/java/com/team4/atlas/VScroll.java
@@ -0,0 +1,30 @@
+package com.team4.atlas;
+
+/**
+ * Created by Victor on 2/03/2018.
+ */
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.widget.ScrollView;
+
+public class VScroll extends ScrollView {
+
+    public VScroll(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public VScroll(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public VScroll(Context context) {
+        super(context);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        return false;
+    }
+}
